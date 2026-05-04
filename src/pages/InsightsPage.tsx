dc708239b2c27d28da6533b9bb9e99d54dcaa6e5
draxmit <allensiusf@gmail.com>
2026-05-04 19:36:23 +0700

    chore(T1): archive Vite prototype to legacy/

--- src/pages/InsightsPage.tsx
+++
@@ -1,8 +0,0 @@
-import { BehavioralInsightsCard } from '../components/insights/BehavioralInsightsCard'
-import { INSIGHTS } from '../lib/mock-data'
-import { useReveal } from '../hooks/useReveal'
-
-export function InsightsPage() {
-  useReveal()
-  return <BehavioralInsightsCard insights={INSIGHTS} />
-}
